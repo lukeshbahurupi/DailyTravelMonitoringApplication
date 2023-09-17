0ea7c622b7f43e2fe483d2aee4f86352c49f2072
Lukesh Bahurupi <lbahurupi567@gmail.com>
2023-09-18 00:25:45 +0530

    Add project files.

---
+++ DailyTravelMonitoringApplication/Controllers/EmployeeController.cs
@@ -0,0 +1,59 @@
+﻿using DailyTravelMonitoringApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DailyTravelMonitoringApplication.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly TravelingTeam_DB_Context _dbContext = new TravelingTeam_DB_Context();
+        
+        // GET: Employee
+        public ActionResult Index()
+        {            
+            return View(_dbContext.Employees.ToList());
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Employee value)
+        {
+            List<DailyTravelMonitoring> list = new List<DailyTravelMonitoring>();
+            if (ModelState.IsValid)
+            {
+                DailyTravelMonitoring obj = new DailyTravelMonitoring();
+                list.Add(obj);
+                value.DailyTravelMonitorings = list;
+                _dbContext.Employees.Add(value);
+                _dbContext.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        public ActionResult Delete(int? id)
+        {
+
+            return View(_dbContext.Employees.Find(id));
+        }
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult GetDelete(int? id)
+        {
+            if (id != null)
+            {
+                Employee obj = _dbContext.Employees.Find(id);
+
+                _dbContext.Employees.Remove(obj);
+                _dbContext.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(_dbContext.Employees.Find(id));
+        }
+        
+    }
+}
---
+++ DailyTravelMonitoringApplication/Models/PEmployee.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DailyTravelMonitoringApplication.Models
+{
+    [MetadataType(typeof(EmployeeMetadataType))]
+    public partial class Employee
+    {
+    }
+    public class EmployeeMetadataType
+    {
+        [Required]
+        public int EmployeeId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Gender { get; set; }
+        [Required]
+        public System.DateTime DateOfBirth { get; set; }
+        [Required(ErrorMessage = "Please enter a value equal to 10 digit.")]
+        [MinLength(10)]
+        [MaxLength(10)]
+        public string MobileNo { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public System.DateTime DateOfJoining { get; set; }
+        [Required(ErrorMessage = "Please enter a value equal to 12 digit.")]
+        [MinLength(12)]
+        [MaxLength(12)]
+        public string UAN_No { get; set; }
+        [Required]
+        public string ActiveFlag { get; set; }
+    }
+}
